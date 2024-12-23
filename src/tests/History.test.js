c038fa13b0430b725a6e76c7fa689933d6e36bad
giovanibaldan <giovani-fox1@hotmail.com>
2024-12-23 13:41:42 -0300

    Adicionando configurações e estrutura inicial para os testes de History.vue

--- src/tests/History.test.js
+++ src/tests/History.test.js
@@ -1,26 +1,49 @@
-import History from "../components/Game.vue";
-import { mount } from "@vue/test-utils";
-import { expect } from "vitest";
-import { createRouter, createWebHistory } from 'vue-router'
+import History from "../components/History.vue";
+import { mount, flushPromises } from "@vue/test-utils";
+import { createRouter, createWebHistory } from "vue-router";
 import { routes } from "../router";
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
 
 function historyFactory() {
     return mount(History, {
         global: {
             plugins: [router]
-        }
-    })
+        },
+    });
 }
 
-describe('History.vue', () => {
-    it('renders the history table body', () => {
-        const wrapper = historyFactory()
-        // expect(wrapper.find(''))
+const MOCK_GAMES = [
+    { id: 1, winner: 'O', game_state: ['X', 'O', 'X', 'O', 'O', 'X', 'X', 'O', ''], created_at: '2024-12-04T16:11:38.865Z', updated_at: '2024-12-04T16:12:07.237Z' },
+    { id: 2, winner: 'O', game_state: ['X', 'X', 'O', '', 'X', 'O', '', '', 'O'], created_at: '2024-12-04T16:11:47.147Z', updated_at: '2024-12-09T19:38:56.622Z' },
+    { id: 4, winner: 'X', game_state: ['X', 'O', 'X', 'O', 'O', 'X', 'X', 'O', ''], created_at: '2024-12-05T14:50:59.816Z', updated_at: '2024-12-05T14:50:59.816Z' },
+    { id: 5, winner: 'X', game_state: ['X', 'O', 'O', 'O', 'X', 'X', 'X', '', 'X'], created_at: '2024-12-04T16:11:49.405Z', updated_at: '2024-12-04T16:15:33.372Z' },
+    { id: 7, winner: 'Empate', game_state: ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'O'], created_at: '2024-12-04T16:11:51.149Z', updated_at: '2024-12-04T16:16:48.965Z' }
+]
 
-    })
-})
+describe('History.vue', () => {
+    let fetchMock;
+
+    beforeEach(() => {
+        fetchMock = vi.fn().mockResolvedValue({
+            json: vi.fn().mockResolvedValue(MOCK_GAMES),
+        });
+        global.fetch = fetchMock;
+    });
+
+    it('renders the history table body', async () => {
+        const wrapper = historyFactory();
+
+        // Aguarda todas as promises e renderizações serem resolvidas
+        await flushPromises();
+
+        expect(fetchMock).toHaveBeenCalledWith('http://localhost:3000/games', { method: 'GET' });
+
+        const rows = wrapper.findAll('tr.table-game-row');
+        expect(wrapper.find('.table-main').exists()).toBe(true);
+        expect(rows.length).toBe(5);
+    });
+});
